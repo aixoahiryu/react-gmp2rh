6c486f6b1923c1da5335312434acf92e53caadc0
aixoahiryu <aixoahiryu@gmail.com>
2019-06-04 16:21:10 +0700

    2 files modified

--- index.js
+++ index.js
@@ -23,4 +23,20 @@ class App extends Component {
   }
 }
 
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      React.createElement("div", {
+        className: "shopping-list"
+      },
+      React.createElement("h1", null, "Shopping List for "),
+      React.createElement("ul",null,
+        React.createElement("li", null, "Instagram"),
+        React.createElement("li", null, "WhatsApp"),
+        React.createElement("li", null, "Oculus")))
+    );
+  }
+}
+
 render(<App />, document.getElementById('root'));
+render(<ShoppingList />, document.getElementById('shopping'));
